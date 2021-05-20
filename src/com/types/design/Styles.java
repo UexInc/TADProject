97369db96238fc1eea7bad5ebda92e5f19e7d67f
UexInc <78887757+UexInc@users.noreply.github.com>
2021-05-20 02:05:22 -0300

    Appearance update

--- src/com/types/design/Styles.java
+++ src/com/types/design/Styles.java
@@ -2,6 +2,9 @@ package com.types.design;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -13,26 +16,22 @@ import javax.swing.JTextField;
 import com.types.gui.Main;
 
 public final class Styles {
-	// Cores TEMA de fundo
+	// Cores TEMA - fundo
 	public static Color colorTheme = new Color(81, 0, 148);
 	public static Color colorTheme_dark = new Color(59, 0, 107);
 	public static Color colorTheme_tooDark = new Color(38, 0, 69);
 	public static Color colorTheme_light = new Color(115, 0, 209);
 	public static Color colorTheme_tooLight = new Color(221, 179, 255);
-	// Cores TEMA de texto
+	// Cores TEMA - texto
 	public static Color colorTheme_textLight = new Color(221, 179, 255);
 	// Outras cores
-	public static Color colorTextDarkGrey = new Color(51, 51, 51);
-	public static Color colorTextLightGrey = new Color(200, 200, 200);
 
 	public static void setStyleTable(JTable table) {
 		table.getTableHeader().setDefaultRenderer(new Filters.HeaderRenderer());
 		table.getTableHeader().setBackground(colorTheme_tooLight);
-		table.getTableHeader().setForeground(colorTextDarkGrey);
 		for (int i = 0; i < table.getColumnCount(); i++) {
 			table.getColumnModel().getColumn(i).setCellRenderer(new Filters.HTMLRenderer());
 		}
-
 		table.setBackground(colorTheme);
 		table.setForeground(colorTheme_textLight);
 		table.setBorder(BorderFactory.createLineBorder(null));
@@ -42,7 +41,7 @@ public final class Styles {
 	public static void setStyleScrollPanel(JScrollPane scroll) {
 		scroll.getViewport().setBackground(colorTheme_tooLight);
 	}
-	
+
 	public static void setStyleModelPanel(JPanel panel) {
 		panel.setBackground(colorTheme);
 	}
@@ -67,21 +66,18 @@ public final class Styles {
 	}
 
 	public static void setStyleLabel(JLabel label) {
+		label.setFont(new Font("Arial", Font.BOLD, Main.SIZE.height / 60)); // fonte
 		label.setForeground(colorTheme_textLight);
 	}
 
-	public static void setStyleField(JTextField jTextField) {
-		// TODO Auto-generated method stub
-
+	public static void setStyleField(JTextField field) {
+		field.setFont(new Font("Arial", Font.BOLD, Main.SIZE.height / 60)); // fonte
+		field.setBorder(null);
+		field.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 	}
 
 	public static String rowRender(String r) {
-		String font = "<style>"
-				+ "p{"
-				+ "font-family:\"Impact\";"
-				+ "font-weight:normal;"
-				+ "}"
-				+ "</style>";
+		String font = "<style>" + "p{" + "font-family:\"Impact\";" + "font-weight:normal;" + "}" + "</style>";
 		return "<html>" + font + "<p>" + r + "</p></html>";
 	}
 
